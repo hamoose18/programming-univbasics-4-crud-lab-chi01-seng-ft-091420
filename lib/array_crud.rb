7afbc5ed56dd1f8b124f56288f03c70e4a622666
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-04 09:56:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,45 @@
 def create_an_empty_array
-  
+[]  
+
 end
 
 def create_an_array
+  [:I, :am, :really, :learning]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array = [:wow, :I, :am, :really, :learning]
+array << "arrays!"
+p array
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+array = [:wow, :I, :am, :really, :learning]
+array.unshift "wow"
+p array
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+array = [:I, :am, :really, :learning, :arrays!]
+array.pop #-> "arrays!"
+p array
+p "arrays!"
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+array = [:wow, :I, :am, :really, :learning, :arrays!]
+array.shift #-> "wow"
+p array
+p "wow"
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
